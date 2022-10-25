bdaebee4a6079d21182f7e3d6defb8a88ee69f25
alexandre cabral <caaarolinejrs@gmail.com>
2022-10-25 17:33:18 -0300

    commit inicial

---
+++ CRUDWebApplication/CRUDWebApplication/Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace CRUDWebApplication.Domain.Entities
+{
+    public class User : Contact
+    {
+        public int id { get; set; }
+        public int CPF { get; set; }
+        public string name { get; set; }
+
+        public string email { get; set; }
+        public int CEP { get; set; }
+        public string street { get; set; }
+        public string complement { get; set; }
+
+        public string district { get; set; }
+        public string UF { get; set; }
+        List<Contact> contacts { get; set; }
+
+    }
+}
